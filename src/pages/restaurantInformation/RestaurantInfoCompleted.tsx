5a9f3fdf32a633e19bb717c7d3b5bf35b37b3880
Jan Domanski <jan.lean@Jans-MBP-702.fritz.box>
2023-12-17 11:56:30 +0100

    Add userIcon to the restaurantInfoCompletedPage

--- src/pages/restaurantInformation/RestaurantInfoCompleted.tsx
+++ src/pages/restaurantInformation/RestaurantInfoCompleted.tsx
@@ -1,11 +1,13 @@
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import {ReactComponent as UserLogo} from '../../Icons_Basic_User.svg'
 
 export function RestaurantInfoCompleted () {
     const navigate = useNavigate();
     return (
 
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh", padding: "1rem" }}>
+            <UserLogo/>
             <h2 style={{ textAlign: "center" }}>You are all set!</h2>
             <p style={{ textAlign: "center" }}>Congratulations! You are now part of the Rasa Malang community. Tourists from all around the world will discover your restaurant.</p>
             <Button style={{marginTop:"2rem"}}variant="contained" onClick={() => navigate("/restaurant")}>Go to homepage</Button>
